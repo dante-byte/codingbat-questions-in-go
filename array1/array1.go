3f6570ff43c038d0724dda90a948f328e181faff
Dante White <559.dantewhite@gmail.com>
2020-09-06 17:29:33 -0400

    array-1

--- array1/array1.go
+++ array1/array1.go
@@ -199,7 +199,219 @@ func maxEnd3(nums []int) []int {
 
 }
 
+func sum2(nums []int) int  {
 
+	/*
+	Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+	sum2([1, 2, 3]) → 3
+	sum2([1, 1]) → 2
+	sum2([1, 1, 1, 1]) → 2
+	 */
+
+	n := 0
+
+	rult := 0
+
+	if len(nums) == 0 {
+
+		return 0
+	} else if len(nums) == 1 {
+
+		return nums[0]
+
+	}
+
+	for n < 2 {
+
+		rult += nums[n]
+		n++
+
+	}
+
+
+	return rult
+
+}
+
+func middleWay(a []int, b []int) []int  {
+
+	/*
+	Given 2 int arrays, a and b, each length 3,
+	return a new array length 2 containing their
+	middle elements.
+
+	middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
+	middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
+	middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
+	 */
+
+	n := len(a) / 2
+
+
+
+	rult := []int{a[n],b[n]}
+
+	return rult
+
+}
+
+func makeEnds(nums []int) []int {
+
+
+	/*
+	Given an array of ints, return a new
+	array length 2 containing the first
+	and last elements from the original
+	array. The original array will be length
+	1 or more.
+
+	makeEnds([1, 2, 3]) → [1, 3]
+	makeEnds([1, 2, 3, 4]) → [1, 4]
+	makeEnds([7, 4, 6, 2]) → [7, 2]
+	 */
+
+	rult := []int{nums[0],nums[len(nums)-1]}
+
+	return rult
+
+}
+
+func has23(nums []int) bool {
+
+	/*
+	Given an int array length 2,
+	return true if it contains a
+	2 or a 3.
+
+	has23([2, 5]) → true
+	has23([4, 3]) → true
+	has23([4, 5]) → false
+	 */
+
+	if nums == nil {
+
+		return false
+	}
+
+	i := 0
+
+	for i < len(nums) {
+
+		if nums[i] == 2 || nums[i] == 3 {
+
+			return true
+
+		}
+		i++
+	}
+
+	return false
+
+}
+
+
+func no23(nums []int) bool {
+
+	/*
+	Given an int array length 2, return
+	true if it does not contain a 2 or 3.
+
+	no23([4, 5]) → true
+	no23([4, 2]) → false
+	no23([3, 5]) → false
+	 */
+
+	if nums == nil {
+
+		return false
+
+	}
+
+	var i int = 0
+
+	for i < len(nums) {
+
+		if nums[i] == 2 || nums[i] == 3 {
+
+			return false
+		}
+		i++
+	}
+
+	return true
+}
+
+
+func makeLast(nums []int) []int {
+
+	/*
+	Given an int array, return a new
+	array with double the length where
+	its last element is the same as the
+	original array, and all the other
+	elements are 0. The original array will
+	be length 1 or more. Note: by default,
+	a new int array contains all 0's.
+
+	makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+	makeLast([1, 2]) → [0, 0, 0, 2]
+	makeLast([3]) → [0, 3]
+	 */
+
+
+
+	rult := make([]int,len(nums) * 2)
+
+	rult[len(rult)-1] = nums[len(nums)-1]
+
+	return rult
+}
+
+func double23(nums []int) bool {
+
+	/*
+	Given an int array, return true if
+	the array contains 2 twice, or 3 twice.
+	The array will be length 0, 1, or 2.
+
+	double23([2, 2]) → true
+	double23([3, 3]) → true
+	double23([2, 3]) → false
+	 */
+
+	var two int = 0
+	var three int = 0
+
+	i := 0
+
+	for i < len(nums) {
+
+		if nums[i] == 2 {
+
+			two++
+
+		}
+
+		if nums[i] == 3 {
+
+			three++
+
+		}
+		i++
+	}
+
+	if two >= 2 || three >= 3 {
+
+		return true
+
+	}
+
+	return false
+
+
+
+}
 
 
 
