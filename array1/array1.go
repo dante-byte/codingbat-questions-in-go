ce4e6aa91a0efe3df492512f297de171ad5d18e9
Dante White <559.dantewhite@gmail.com>
2020-09-06 05:01:15 -0400

    coding bat

--- array1/array1.go
+++ array1/array1.go
@@ -24,8 +24,47 @@ func firstLast6(nums []int) bool {
 
 	return false
 
+}
 
+
+func sameFirstLast(nums []int) bool  {
+
+	/*
+
+	Given an array of ints, return true if
+	the array is length 1 or more, and the
+	first element and the last element are
+	equal.
+
+	 */
+
+	if len(nums) < 1 {
+
+		return false
+
+	}
+
+	if nums[0] == nums[len(nums)] {
+
+		return true
+	}
+
+	return false;
+}
+
+func makePi() []int {
+
+	rult := make([]int,3)
+
+	rult[0] = 3
+	rult[1] = 1
+	rult[2] = 4
+
+	return rult;
 
 
 
 }
+
+
+
