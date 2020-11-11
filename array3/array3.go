5d584d03d6d726991350ed819d4c22c64665be0b
Dante White <559.dantewhite@gmail.com>
2020-11-11 06:52:04 -0500

    logic1

--- array3/array3.go
+++ array3/array3.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	$END$
+
+	fmt.Println("hello")
+
+
+
+	
+}
+
+func maxSpan(nums []int) int {
+
+	/*
+
+	Consider the leftmost and righmost
+	appearances of some value in an
+	array. We'll say that the "span"
+	is the number of elements between
+	the two inclusive. A single value
+	has a span of 1. Returns the
+	largest span found in the given
+	array. (Efficiency is not a priority.)
+
+
+	maxSpan([1, 2, 1, 1, 3]) → 4
+	maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+	maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+	 */
+
+	max := 0
+
+	if len(nums) == 1 {
+		return 1
+	}
+
+	for i := 0; i < len(nums); i++ {
+
+		cur := nums[i]
+
+		for j := len(nums)-1; j > 0; j-- {
+
+			if nums[j] == cur {
+
+				max = int(math.Max(float64(max), float64(j-i+1)))
+
+			}
+		}
+	}
+
+	return max
 }
